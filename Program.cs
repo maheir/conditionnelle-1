d59d00d946c4e6b2b0046544c4c56617fbad9033
maheir <maheirne@gmail.com>
2018-12-19 16:16:09 +0100

    conditionnelle 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionnelle_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rainy = false;
+            if (rainy)
+                Console.WriteLine("Bonjour, il ne pleut pas aujourd’hui, bonne journée.");
+            else
+                Console.WriteLine("Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie.");
+                
+        }
+    }
+}
